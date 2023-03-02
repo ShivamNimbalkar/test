8e3d47e0014cf7d9b46356dad35d4555a57ea28b
SHIVAM GAJANAN NIMBALKAR <SHIVAMNIMBALKAR3901@GMAIL.COM>
2023-03-02 23:58:40 +0530

    added this

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import MusicPlayer from "./player/MusicPlayer";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./componants/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <div className="main">
         <Navbar />
         <App />
-      </div>
-    </BrowserRouter>
+      </div> */}
+    {/* </BrowserRouter> */}
+    <MusicPlayer />
   </React.StrictMode>
 );
